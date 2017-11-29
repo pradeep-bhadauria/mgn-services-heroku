60eb0d74e238d22f9ac2ffab389b39ca5229b753
Pradeep Bhadauria <pradeep.bhadauria08@gmail.com>
2017-11-29 09:29:52 +0700

    Git repo change commit - first

---
+++ config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+
+_basedir = os.path.abspath(os.path.dirname(__file__))
+
+DEBUG = False
+#SERVER_NAME = 'localhost:5000'
+PREFERRED_URL_SCHEME = 'http'
+
+SECRET_KEY = 'this-really-needs-to-be-changed'
+
+SQLALCHEMY_DATABASE_URI = 'postgres://jcepytxffjskun:heHyQX9CwinC9EpJCZIceXAGR4@ec2-54-163-239-63.compute-1.amazonaws.com:5432/d241q24dttbkg1'
+SQLALCHEMY_ECHO = True
+DATABASE_CONNECT_OPTIONS = {}
